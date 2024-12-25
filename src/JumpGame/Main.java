912c33750775c48ba879d289187dee1e63c54a12
JackieWeckie <nekit4568i@gmail.com>
2024-12-25 21:50:05 +0300

    added java docs

--- src/JumpGame/Main.java
+++ src/JumpGame/Main.java
@@ -1,5 +1,11 @@
 package JumpGame;
 
+/**
+ * You are given an integer array nums. You are initially positioned at the array's first index,
+ * and each element in the array represents your maximum jump length at that position.
+ * Return true if you can reach the last index, or false otherwise.
+ */
+
 public class Main {
     public static void main(String[] args) {
         int[] nums = {2,3,1,1,4};
