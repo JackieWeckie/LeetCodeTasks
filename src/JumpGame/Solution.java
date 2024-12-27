744133b38784003d4b18a3eb0bd25ee5ac84dc89
JackieWeckie <nekit4568i@gmail.com>
2024-12-27 11:16:11 +0300

    little changes 27/12/2024

--- src/JumpGame/Solution.java
+++ src/JumpGame/Solution.java
@@ -10,4 +10,9 @@ public class Solution {
         }
         return true;
     }
+
+    public static void main(String[] args) {
+        int[] nums = {2,3,1,1,4};
+        System.out.println(Solution.canJump(nums));
+    }
 }
